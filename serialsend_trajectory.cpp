de02a653b210da92c3ad46cd06030ce3270a01c7
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2019-11-01 17:02:03 -0600

    comms working, develop into full controller now.

--- serialsend_trajectory.cpp
+++ serialsend_trajectory.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include <string.h>
+#include <cstring>
+#include <cerrno>
+#include <iostream>
+#include <fstream>
 
 //Linux Headers
 #include <unistd.h> //unix standard function defs read() write() open
@@ -7,30 +10,82 @@
 #include <errno.h> //Error functions
 #include <termios.h> //POSIX Terminal Controls
 
+// C library headers
+#include <stdio.h>
+#include <string.h>
 
-int open_port(string port_name, int baudrate){
-
-  int ser_port = open(port_name, O_RDWR);
 
 
-	if(ser_port < 0){ // If open fails
-		printf("Error %i from open %s\n", errno, strerror(errno));
-	}
+#define MSG_SIZE 19 //sum of 4 char long angles and delimiters
+
+
+using namespace std;
+
+// void write2port(int ser_port, string message){
+//   unsigned char msg_arr[MSG_SIZE];
+//   write(ser_port, message, sizeof(msg_arr));
+// }
+
+int main(){
+
+  int arduino_usb = open("/dev/ttyACM0", O_RDWR);
 
   struct termios tty;
+  struct termios tty_old;
 
-  memset(&tty, 0, sizeof tyy);
-
-  if(tcgetattr(ser_port, &tty) != 0){
-    printf("Error %i from tcggetattr: %s\n", errno, strerror(errno));
+  if ( tcgetattr ( arduino_usb, &tty ) != 0 ) {
+   std::cout << "Error " << errno << " from tcgetattr: " << std::strerror(errno) << std::endl;
   }
 
-  //Set Serial Comms to arduino defaults
-  tty.c_cflag &= ~PARENB; //disable parity bit
-  tty.c_cflag &= ~CSTOPB; //one stop bit
-  tty.c_cflag |= CS8; //8 bits per byte
+  tty_old = tty;
+
+  cfsetospeed (&tty, (speed_t)B115200);
+  cfsetispeed (&tty, (speed_t)B115200);
+
+  tty.c_cflag &= ~PARENB;
+
+  unsigned char msg[MSG_SIZE];
+  int settle_time = 4; //implement a send back to confirm stabilization 
+
+  write(arduino_usb, "0600,0600,0600,0600", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0800,0800,0800,0800", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0600,0600,0600,0600", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0512,0512,0512,0512", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0512,612,0512,412", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0512,612,0512,412", sizeof(msg));
+  sleep(settle_time);
+  write(arduino_usb, "0512,412,0512,612", sizeof(msg));
+  sleep(settle_time);
 
 
 
-	return(ser_port);
+
+  // write(arduino_usb, "0512,0512,0512,0512", sizeof(msg));
+  // write(arduino_usb, "0512,612,0512,412", sizeof(msg));
+  // write(arduino_usb, "0612,0512,0412,0512", sizeof(msg));
+
+
+  // int n = read(arduino_usb,&string_buff,256);
+  // std::cout<<string_buff;
+
+
+
+  //}
+
+
+  // char c = 'a';
+  //  while(c != 'q'){
+  //    cin >> c;
+  //    int n = read(arduino_usb,&string_buff,256);
+  //    std::cout<<string_buff;
+  //  }
+
+  close(arduino_usb);
+  printf("done\n");
+  return 0;
 }
